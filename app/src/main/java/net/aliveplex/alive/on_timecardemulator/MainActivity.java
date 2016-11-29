e268b577ca85c105581346ba652ac9d1629577ba
team28079 <team700@hotmail.com>
2016-11-29 22:01:52 +0700

    sharePreference update
    ใส่ sharePreference มาและเมนูปิดแอบที่มุมขวาบนของ UI
    เพิ่มคลาสและ xml ที่จำเป็น

--- app/src/main/java/net/aliveplex/alive/on_timecardemulator/MainActivity.java
+++ app/src/main/java/net/aliveplex/alive/on_timecardemulator/MainActivity.java
@@ -1,13 +1,44 @@
 package net.aliveplex.alive.on_timecardemulator;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sp = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int mu = item.getItemId();
+        switch (mu){
+            case R.id.menuSetting :
+                Intent settingIntent = new Intent(MainActivity.this,myPreference.class);
+                startActivity(settingIntent);
+                break;
+            case R.id.menuExit :
+                Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                homeIntent.addCategory(Intent.CATEGORY_HOME);
+                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+                break;
+        }
+        return true;
     }
 }
